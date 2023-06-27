85bb40e32d8109eccc9e67b8b4fdb21b574402e5
Atommation <111581040+Atommation@users.noreply.github.com>
2023-06-27 19:02:29 +0800

    Add files via upload

---
+++ v2.2.5/CalculateGame_v2.2.5.go
@@ -0,0 +1,118 @@
+package main
+
+//all of the following are standard libs
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+// main data structure including random datas
+type RandomStruct struct {
+	Number1            int
+	Number2            int
+	FigureMethod       int
+	Answer             int
+	FigureMethodSymbol string
+}
+
+// a method to produce random data for RandomStruct
+func (r *RandomStruct) UpdateData() {
+
+	rand.Seed(time.Now().UnixNano())
+	//0:add 1:sub 2:times 3:divide
+	r.FigureMethod = int(rand.Intn(4))
+
+	switch r.FigureMethod {
+	//add
+	case 0:
+		r.Number1 = rand.Intn(100)
+		r.Number2 = rand.Intn(100)
+		r.Answer = int(r.Number1 + r.Number2)
+		r.FigureMethodSymbol = "+"
+	//sub
+	case 1:
+		r.Number2 = rand.Intn(100)
+		r.Number1 = rand.Intn(100) + r.Number2
+		r.Answer = int(r.Number1 - r.Number2)
+		r.FigureMethodSymbol = "-"
+	//times
+	case 2:
+		r.Number1 = rand.Intn(20)
+		r.Number2 = rand.Intn(10)
+		r.Answer = int(r.Number1 * r.Number2)
+		r.FigureMethodSymbol = "×"
+	//divide
+	case 3:
+		r.Number2 = rand.Intn(10) + 1
+		r.Number1 = rand.Intn(20) * r.Number2
+		r.Answer = int(r.Number1 / r.Number2)
+		r.FigureMethodSymbol = "÷"
+	}
+
+}
+
+// main data struct including input, grade, etc.
+type UserStruct struct {
+	TotalTime         int
+	RightTime         int
+	InputNumber       string
+	InputNumber_int64 int64
+	Over              bool
+}
+
+// a method of processing
+func (u *UserStruct) UpdateData(data RandomStruct) {
+
+	//show the task
+	fmt.Printf("\n\n第%d题, %d %s %d = ", u.TotalTime+1, data.Number1, data.FigureMethodSymbol, data.Number2)
+
+	//get data from input
+	fmt.Scanln(&u.InputNumber)
+	//convert to int64 in order to compare later
+	u.InputNumber_int64, _ = strconv.ParseInt(u.InputNumber, 10, 64)
+
+	//compare input with right answer
+	if int(u.InputNumber_int64) == data.Answer {
+		u.RightTime++
+		u.TotalTime++
+		fmt.Printf("\n回答正确")
+	} else if u.InputNumber == "999" {
+		//mark to stop
+		u.Over = true
+	} else {
+		fmt.Printf("\n回答错误")
+		u.TotalTime++
+	}
+
+	return
+}
+
+func main() {
+	for {
+		//prepare data
+		var RandomData RandomStruct
+		var UserData UserStruct
+		var Rate float32
+
+		//show instructions
+		fmt.Println("游戏开始，输入\"999\"退出并查看答题情况")
+		time.Sleep(time.Second*2)
+
+		for {
+			//reset data
+			RandomData.UpdateData()
+			UserData.UpdateData(RandomData)
+
+			if UserData.Over == true {
+				Rate = (float32(UserData.RightTime) / float32(UserData.TotalTime)) * 100
+				fmt.Printf("\n\n游戏结束，您答对了%d题，共%d题，正确率 %2.2f%%\n\n\n", UserData.RightTime, UserData.TotalTime, Rate)
+				break
+			}
+		}
+
+		time.Sleep(time.Second * 2)
+	}
+
+}
